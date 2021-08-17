26a58d047e33b10d9762a5a06e508993743fa2fb
jimena <jimenahernando@hotmail.com>
2021-08-17 20:39:36 +0200

    Agrego componentes: form, listaProductos, totalCompra. Importo lib FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { FormComponent } from './components/form/form.component';
+import { ListaProductosComponent } from './components/lista-productos/lista-productos.component';
+import { TotalCompraComponent } from './components/total-compra/total-compra.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    ListaProductosComponent,
+    TotalCompraComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/total-compra/total-compra.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-total-compra',
+  templateUrl: './total-compra.component.html',
+  styleUrls: ['./total-compra.component.css']
+})
+export class TotalCompraComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
