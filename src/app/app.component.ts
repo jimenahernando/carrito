9464b73dff16182338444e4d946630ae81f0b65d
jimena <jimenahernando@hotmail.com>
2021-08-18 14:07:26 +0200

    OUTPUT de form a appComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Producto } from './interfaces/producto.interface';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'carrito';
+
+  listaProductos: Producto[];
+
+  constructor(){
+    this.listaProductos = [
+      {
+        producto: 'lejia',
+        precio: 2.30,
+        cantidad: 32,
+      }
+    ];
+    // console.log('appcomponent',this.listaProductos);
+  }
+
+  ngOnInit(): void {
+    // console.log(this.listaProductos);
+  }
+
+  onProductoCreado($event: any){
+    // para corroborar que me llega el objeto
+    // console.log($event);
+    this.listaProductos.push($event);
+    
+    console.log(this.listaProductos);
+  }
 }
--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Producto } from 'src/app/interfaces/producto.interface';
 
 @Component({
   selector: 'app-form',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
-  constructor() { }
+  // Solucion conun objeto
+  producto: Producto;
+
+  @Output() productoCreado: EventEmitter<Producto>;
+
+  constructor() {
+    this.producto = {
+      producto: "",
+      precio: 0,
+      cantidad: 0,
+    } 
+
+    this.productoCreado = new EventEmitter();
+  }
 
   ngOnInit(): void {
   }
 
+  cargarProducto(){
+    this.productoCreado.emit(this.producto);
+    // lo vacio a la espera que se le asigne un nuevo valor
+    this.producto = { producto:"", precio:0, cantidad:0 };
+    //console.log(this.producto);
+  }
 }
