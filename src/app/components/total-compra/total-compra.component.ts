f3eef4d7941cc07e5237406ae31d83d0e2d60448
jimena <jimenahernando@hotmail.com>
2021-08-18 15:11:01 +0200

    INPUT totalCompra.:lipstick: quito clase y uso etiqueta HTML

--- src/app/components/total-compra/total-compra.component.ts
+++ src/app/components/total-compra/total-compra.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Producto } from 'src/app/interfaces/producto.interface';
 
 @Component({
   selector: 'app-total-compra',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TotalCompraComponent implements OnInit {
 
-  constructor() { }
+  @Input() productos: Producto[];
+
+  constructor() {
+    this.productos = [];
+  }
 
   ngOnInit(): void {
   }
 
+  mostrarTotal(){
+    let result = "";
+    let total = 0
+    this.productos.forEach(producto => {
+      total += producto.cantidad * producto.precio;
+    })
+
+    //Solucion Profe: hay que controlar que tenga algo el array
+    // let mapaPrecios = this.productos.map(producto => producto.precio * producto.cantidad );
+    // if(mapaPrecios.length != 0){
+    //   let total = mapaPrecios.reduce((precioAnt, precioSig) => precioAnt + precioSig);
+    // }
+    
+    return `<h3>Total de la Compra: ${total}€</h3>`;
+  }
 }
